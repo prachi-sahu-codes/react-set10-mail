c069c2dea0b408cf70a7e4df405bd3ffb438bc25
SahuPrachi <prachisahu998@gmail.com>
2023-04-22 01:54:37 +0530

    media query added

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,19 +1,15 @@
 import { NavLink } from "react-router-dom";
 
 export const Header = () => {
-  const activeStyle = ({ isActive }) => ({
-    borderRight: isActive ? "4px solid black" : "",
-    color: isActive ? "red" : "gray",
-  });
   return (
     <nav className="sidebar">
-      <NavLink to="/" style={activeStyle} className="navLink">
+      <NavLink to="/" className="navLink">
         Inbox
       </NavLink>
-      <NavLink to="/spam" style={activeStyle} className="navLink">
+      <NavLink to="/spam" className="navLink">
         Spam
       </NavLink>
-      <NavLink to="/trash" style={activeStyle} className="navLink">
+      <NavLink to="/trash" className="navLink">
         Trash
       </NavLink>
     </nav>
